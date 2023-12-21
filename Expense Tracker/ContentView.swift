6b5235930e9a1364f720e4e50488b7ea2857f82f
Imam Robani <imamrob@gmail.com>
2023-12-21 09:28:09 +0700

    :hammer: implement setting and search page

--- Expense Tracker/ContentView.swift
+++ Expense Tracker/ContentView.swift
@@ -8,35 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
-    // Intro Visibility status
+    /// Intro Visibility status
     @AppStorage("isFirstTime") private var isFirstTime: Bool = true
-    // Active Tab
+    /// App  Lock Properties
+    @AppStorage("isAppLockEnable") private var isAppLockEnable: Bool = false
+    @AppStorage("lockWhenAppGoesBackground") private var lockWhenAppGoesBackground: Bool = false
+    /// Active Tab
     @State private var activeTab: Tab = .recents
-
     var body: some View {
-        TabView(selection: $activeTab) {
-            Recents()
-                .tag(Tab.recents)
-                .tabItem { Tab.recents.tabContent }
-            
-            Search()
-                .tag(Tab.search)
-                .tabItem { Tab.search.tabContent }
-            
-            Graphs()
-                .tag(Tab.charts)
-                .tabItem { Tab.charts.tabContent }
-            
-            Settings()
-                .tag(Tab.settings)
-                .tabItem { Tab.settings.tabContent }
+        LockView(lockType: .biometric, lockPin: "", isEnabled: isAppLockEnable, lockWhenAppGoesBackground: lockWhenAppGoesBackground) {
+            TabView(selection: $activeTab) {
+                Recents()
+                    .tag(Tab.recents)
+                    .tabItem { Tab.recents.tabContent }
+                
+                Search()
+                    .tag(Tab.search)
+                    .tabItem { Tab.search.tabContent }
+                
+                Graphs()
+                    .tag(Tab.charts)
+                    .tabItem { Tab.charts.tabContent }
+                
+                Settings()
+                    .tag(Tab.settings)
+                    .tabItem { Tab.settings.tabContent }
+            }
+            .tint(appTint)
+            .sheet(isPresented: $isFirstTime, content: {
+                IntroScreen()
+                    .interactiveDismissDisabled()
+            })
         }
-        .tint(appTint)
-        .sheet(isPresented: $isFirstTime, content: {
-            IntroScreen()
-                .interactiveDismissDisabled()
-        })
-        
     }
 }
 
